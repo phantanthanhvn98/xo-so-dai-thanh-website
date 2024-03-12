45e127ac07c43adc752a6afc99063678fb809ac0
Joseph Lopez <josephlopez@gmail.com>
2024-03-12 14:28:42 +0700

    update stream mien bac

--- src/components/live/live/xosomienbac.tsx
+++ src/components/live/live/xosomienbac.tsx
@@ -6,29 +6,29 @@ const XoSoMienBacLive = (props:any) => {
 
     return (
         <div className='bg-[#fff]'>
-            <div className='bg-[#f1bd2166]'>
-                <div className='flex items-center justify-center text-[20px] max-[400px]:text-[16px] text-[#ec222c] font-[500]'>
-                    {`KQXS Miền Bắc -  KQXS ${ketqua.Tinh}`}
+            <div className='bg-[#ec222c]'>
+                <div className='flex items-center justify-center text-[38px] text-[#ffec43] font-[500]'>
+                    {`KQXS Miền Bắc -  KQXS ${ketqua.Tinh} ${ketqua.Ngay.replaceAll("-", "/")}`}
                 </div>
-                <div className='flex items-center justify-center text[16px] max-[400px]:text-[13px] text-[#0073ea] font-[300]'>
+                {/* <div className='flex items-center justify-center text[16px] max-[400px]:text-[13px] text-[#0073ea] font-[300]'>
                     {`XSMB / XSMB ${getDayOfWeekVN(parseDateFromDDMMYYYY(ketqua.Ngay))} / XSMB ${ketqua.Ngay.replaceAll("-", "/")}`}
-                </div>
+                </div> */}
             </div>
             <div className='flex justify-between border-solid border-b-[1px] border-b-[#0000001a]'>
-                <div className='flex w-10 items-center justify-center text-[16px] text-[#000] font-[300] border-l-[1px] border-b-[#0000001a]'>
+                <div className='flex w-10 items-center justify-center min-w-32 text-[38px] font-[500] border-l-[1px] border-b-[#0000001a]'>
                     ĐB
                 </div>
-                <div className='flex w-full items-center justify-center text-[32px] text-[#ec222c] font-[500] border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
+                <div className='flex w-full text-[85px] font-[500] min-h-20 items-center justify-center text-[#ec222c] border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
                     {
                         parseLive(ketqua.KetQua["Giải đặc biệt"][0], ketqua.KetQua["Giải đặc biệt"], ketqua.KetQua["Giải nhất"])
                     }
                 </div>
             </div>
             <div className='flex justify-between border-solid border-b-[1px] border-b-[#0000001a] bg-[#e7f5f2]'>
-                <div className='flex w-10 items-center justify-center text-[16px] text-[#000] font-[300] border-l-[1px] border-b-[#0000001a]'>
+                <div className='flex w-10 items-center justify-center min-w-32 text-[38px] font-[500] border-l-[1px] border-b-[#0000001a]'>
                     1
                 </div>
-                <div className='flex w-full items-center justify-center text-[26px] max-[400px]:text-[24px] text-[#000] font-[500] border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
+                <div className='flex w-full items-center justify-center text-[55px] font-[500] min-h-20 border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
                     {
                         ketqua.KetQua["Giải nhất"].map((item:any, index:number) => {
                             return (
@@ -41,10 +41,10 @@ const XoSoMienBacLive = (props:any) => {
                 </div>
             </div>
             <div className='flex justify-between border-solid border-b-[1px] border-b-[#0000001a]'>
-                <div className='flex w-10 items-center justify-center text-[16px] text-[#000] font-[300] border-l-[1px] border-b-[#0000001a]'>
+                <div className='flex w-10 items-center justify-center min-w-32 text-[38px] font-[500] border-l-[1px] border-b-[#0000001a]'>
                     2
                 </div>
-                <div className='grid grid-cols-2 w-full items-center justify-between text-[26px] max-[400px]:text-[24px] text-[#000] font-[500] border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
+                <div className='grid grid-cols-2 w-full items-center justify-between text-[55px] font-[500] min-h-20  border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
                     {
                         ketqua.KetQua["Giải nhì"].map((item:any, index:number)=> {
                             return (
@@ -57,10 +57,10 @@ const XoSoMienBacLive = (props:any) => {
                 </div>
             </div>
             <div className='flex justify-between border-solid border-b-[1px] border-b-[#0000001a] bg-[#e7f5f2]'>
-                <div className='flex w-10 items-center justify-center text-[16px] text-[#000] font-[300] border-l-[1px] border-b-[#0000001a]'>
+                <div className='flex w-10 items-center justify-center min-w-32 text-[38px] font-[500] border-l-[1px] border-b-[#0000001a]'>
                     3
                 </div>
-                <div className='grid grid-cols-3 w-full items-center justify-between text-[26px] max-[400px]:text-[24px] text-[#000] font-[500] border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
+                <div className='grid grid-cols-3 w-full items-center justify-between text-[55px] font-[500] min-h-20  border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
                     {
                         ketqua.KetQua["Giải ba"].map((item:any, index:number)=> {
                             return (
@@ -73,10 +73,10 @@ const XoSoMienBacLive = (props:any) => {
                 </div>
             </div>
             <div className='flex justify-between border-solid border-b-[1px] border-b-[#0000001a]'>
-                <div className='flex w-10 items-center justify-center text-[16px] text-[#000] font-[300] border-l-[1px] border-b-[#0000001a]'>
+                <div className='flex w-10 items-center justify-center min-w-32 text-[38px] font-[500] border-l-[1px] border-b-[#0000001a]'>
                     4
                 </div>
-                <div className='grid grid-cols-2 w-full items-center justify-between text-[26px] max-[400px]:text-[24px] text-[#000] font-[500] border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
+                <div className='grid grid-cols-2 w-full items-center justify-between text-[55px] font-[500] min-h-20  border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
                     {
                         ketqua.KetQua["Giải tư"].map((item:any, index:number)=> {
                             return (
@@ -89,10 +89,10 @@ const XoSoMienBacLive = (props:any) => {
                 </div>
             </div>
             <div className='flex justify-between border-solid border-b-[1px] border-b-[#0000001a] bg-[#e7f5f2]'>
-                <div className='flex w-10 items-center justify-center text-[16px] text-[#000] font-[300] border-l-[1px] border-b-[#0000001a]'>
+                <div className='flex w-10 items-center justify-center min-w-32 text-[38px] font-[500] border-l-[1px] border-b-[#0000001a]'>
                     5
                 </div>
-                <div className='grid grid-cols-3 w-full items-center justify-between text-[26px] max-[400px]:text-[24px] text-[#000] font-[500] border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
+                <div className='grid grid-cols-3 w-full items-center justify-between text-[55px] font-[500] min-h-20  border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
                     {
                         ketqua.KetQua["Giải năm"].map((item:any, index:number)=> {
                             return (
@@ -105,10 +105,10 @@ const XoSoMienBacLive = (props:any) => {
                 </div>
             </div>
             <div className='flex justify-between border-solid border-b-[1px] border-b-[#0000001a]'>
-                <div className='flex w-10 items-center justify-center text-[16px] text-[#000] font-[300] border-l-[1px] border-b-[#0000001a]'>
+                <div className='flex w-10 items-center justify-center min-w-32 text-[38px] font-[500] border-l-[1px] border-b-[#0000001a]'>
                     6
                 </div>
-                <div className='grid grid-cols-3 w-full items-center justify-between text-[26px] max-[400px]:text-[24px] text-[#000] font-[500] border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
+                <div className='grid grid-cols-3 w-full items-center justify-between text-[55px] font-[500] min-h-20  border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
                     {
                         ketqua.KetQua["Giải sáu"].map((item:any, index:number)=> {
                             return (
@@ -121,14 +121,14 @@ const XoSoMienBacLive = (props:any) => {
                 </div>
             </div>
             <div className='flex justify-between border-solid border-b-[1px] border-b-[#0000001a] bg-[#e7f5f2]'>
-                <div className='flex w-10 items-center justify-center text-[16px] text-[#000] font-[300] border-l-[1px] border-b-[#0000001a]'>
+                <div className='flex w-10 items-center justify-center min-w-32 text-[38px] font-[500] border-l-[1px] border-b-[#0000001a]'>
                     7
                 </div>
-                <div className='grid grid-cols-4 w-full items-center justify-between text-[26px] max-[400px]:text-[24px] text-[#000] font-[500] border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
+                <div className='grid grid-cols-4 w-full items-center justify-between text-[55px] font-[500] min-h-20  border-l-[1px] border-r-[1px] border-b-[#0000001a]'>
                     {
                         ketqua.KetQua["Giải bảy"].map((item:any, index:number)=> {
                             return (
-                                <div className={`flex items-center justify-center`} key={index}>
+                                <div className={`flex items-center justify-center text-[#ec222c]`} key={index}>
                                     {parseLive(item, ketqua.KetQua["Giải bảy"], ['gia-dau'])}
                                 </div>
                             )
